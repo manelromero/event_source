fb3cc15d2f7b7f4c66b69fe4c9c9c9d5b0ea15a6
Manel Romero <manel@manelromero.com>
2023-03-19 12:53:39 +0100

    First commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM ruby:3.2
+
+WORKDIR /app
+
+COPY Gemfile Gemfile.lock ./
+RUN bundle install
+
+COPY . /app
+
+EXPOSE 3000
+
+CMD ["ruby", "sse-server.rb"]
---
+++ sse-server.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'thin'
+
+set :server, 'thin'
+set :port, 3000
+set :bind, '0.0.0.0'
+set :public_folder, 'public'
+
+get '/' do
+  send_file File.join(settings.public_folder, 'index.html')
+end
+
+get '/events', provides: 'text/event-stream' do
+  stream :keep_open do |out|
+    EventMachine::PeriodicTimer.new(1) do
+      message = "Current time: #{Time.now.strftime('%H:%M:%S')}"
+      out << "data: #{message}\n\n"
+    end
+  end
+end
